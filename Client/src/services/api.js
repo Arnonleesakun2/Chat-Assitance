f8e39ce77d49939a2186a158a4ffe02c7653a913
RyeS <Arnonleesakun2@gmail.com>
2025-04-12 18:19:28 +0700

    Update api.js

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://chat-assitance.vercel.app/api';
+const API_BASE_URL = 'https://chat-assitance-server.vercel.app/api';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
